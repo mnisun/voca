35ebc3019f2e3a3be851a5675f85c3904b76067e
mac <sunjw222@naver.com>
2023-05-02 00:04:06 +0900

    인덱스에서 페이지 이동 완성

--- src/main/java/com/word/voca/controller/VocaController.java
+++ src/main/java/com/word/voca/controller/VocaController.java
@@ -11,4 +11,14 @@ public class VocaController {
 
         return "/voca/board.html";
     }
+
+    @GetMapping("/voca")
+    public String voca(){
+        return "/voca/voca.html";
+    }
+
+    @GetMapping("/qna")
+    public String qna(){
+        return "/voca/qna.html";
+    }
 }
