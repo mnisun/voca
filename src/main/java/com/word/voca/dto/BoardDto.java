d46cc0b3e88b19f41a91df421be0a6d1b7f9afcd
mac <sunjw222@naver.com>
2023-05-09 00:31:11 +0900

    데이터베이스 연결 및 lombok설치완료, DTO작성

--- src/main/java/com/word/voca/dto/BoardDto.java
+++ src/main/java/com/word/voca/dto/BoardDto.java
@@ -7,11 +7,58 @@ import java.util.Date;
 @Data
 public class BoardDto {
 
-    private int board_no; // 게시물 번호
+    private int board_no; // 게시물 번호료
     private String title; // 게시물 제목
     private String content; // 게시물 내용
     private String writer; // 게시물 작성자
     private Date reg_date; // 게시물 등록시간
     private int hit; // 조회수
 
+    public int getBoard_no() {
+        return board_no;
+    }
+
+    public void setBoard_no(int board_no) {
+        this.board_no = board_no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public Date getReg_date() {
+        return reg_date;
+    }
+
+    public void setReg_date(Date reg_date) {
+        this.reg_date = reg_date;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public void setHit(int hit) {
+        this.hit = hit;
+    }
 }
